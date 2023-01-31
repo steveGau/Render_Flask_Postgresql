37a1835e8974cf96e4b8da5ecf14182311441159
a290772 <a290772@gmail.com>
2023-01-31 06:16:16 -0500

    Initial Commit Render_Flask_Postgresql

---
+++ app.py
@@ -0,0 +1,33 @@
+# write a python code to create flask app under render.com, Configure flask for PostgreSQL in web host render.com to connect to PostgreSQL, create a table with name "students", insert data to the table , query the data base
+#
+# cd  C:\Users\a2907\Desktop\pyApp\DeepLearning\Render\app_flask_postgresSql
+#
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from psycopg2 import extensions
+#
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@hostname/dbname'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stevegau:pCdaBZ1ehnzaSkF4sQFYgPRWQSWoVl6x@dpg-cfc9gbun6mpiero1lsbg-a/students_0h01'
+db = SQLAlchemy(app)
+#
+class StudentTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+	#
+    def __repr__(self):
+        return '<StudentTable %r>' % self.name
+#
+@app.route('/')
+def index():
+    studentInputData = StudentTable(name='John Doe', age=25)
+    db.session.add(studentInputData)
+    db.session.commit()
+    studentsOutputData = StudentTable.query.all()
+    return str(studentsOutputData)
+#
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
---
+++ app_old.py
@@ -0,0 +1,46 @@
+# cd  C:\Users\a2907\Desktop\pyApp\DeepLearning\Render\app_flask_postgresSql
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from psycopg2 import extensions
+
+app = Flask(__name__)
+                                                   #password=3050Pony$
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:3050Pony$@localhost/students'
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+  __tablename__='students'
+  id=db.Column(db.Integer,primary_key=True)
+  fname=db.Column(db.String(40))
+  lname=db.Column(db.String(40))
+  email=db.Column(db.String(40))
+  
+  def __init__(self,fname,lname,email):
+    self.fname=fname
+    self.lname=lname
+    self.email=email
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+  
+@app.route('/submit', methods=['POST'])
+def submit():
+  fname= request.form['fname']
+  lname=request.form['lname']
+  email=request.form['email']
+  
+  student=Student(fname,lname,email)
+  db.session.add(student)
+  db.session.commit()
+  
+  #fetch a certain student
+  studentResult=db.session.query(Student).filter(Student.id==1)
+  for result in studentResult:
+    print(result.fname)
+    
+  return render_template('success.html', data=fname)
+  
+if __name__ == '__main__':
+    app.run(debug=True)
